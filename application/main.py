80b79dec6a05a638845c72f08bdc5b82eb2b5b8c
Justin Littman <jlittman22@gmail.com>
2017-10-06 10:52:54 -0400

    updated docker-compose

--- application/main.py
+++ application/main.py
@@ -2,9 +2,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/", endpoint='func1')
 def hello_world():
-    return "Hello World!"
+    return "Hello_world"
+
+@app.route("/path", endpoint='func2')
+def func2():
+    return "Hello"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True, port=8080)
