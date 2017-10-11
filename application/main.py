583da1c8f337f6310ff6498168fd6964ff5a94dd
Justin Littman <jlittman22@gmail.com>
2017-10-11 14:27:41 -0400

    updated to python 3, get/post requests

--- application/main.py
+++ application/main.py
@@ -1,10 +1,64 @@
 from flask import Flask
+import logging
+
+import psycopg2
+logging.basicConfig(level=logging.DEBUG)
+logging.warning('Watch out!')
 
 app = Flask(__name__)
+logging.info("pre try")
 
-@app.route("/", endpoint='func1')
-def hello_world():
-    return "Hello_world"
+DB_USER = 'test'
+DB_PASS = 'test'
+DB_NAME = 'test_db'
+DB_HOST = 'postgresql'
+# DB_PORT = '5432'
+
+conn = psycopg2.connect("dbname='{0}' user='{1}' password='{2}' host='{3}'".format(DB_NAME, DB_USER, DB_PASS, DB_HOST))
+cur = conn.cursor()
+
+def create_table(tablename):
+    try:
+        cur.execute("CREATE TABLE IF NOT EXISTS {0}(ID INT PRIMARY KEY NOT NULL)".format(tablename))
+    except psycopg2.OperationalError as e:
+        logging.error('Unable to connect!\n{0}').format(e)
+        pass
+
+@app.route("/<string:tablename>")
+def hello_world(tablename):
+    create_table(tablename)
+    return "Created table: {0}".format(tablename)
+
+@app.route("/<string:tablename>/request", methods = ['GET','POST'])
+def user_data(tablename):
+    create_table(tablename)
+    if request.method == 'GET':
+        try:
+            cur.execute("SELECT * FROM {0}".format(tablename))
+            return "Data retrieved from {0}".format(tablename)
+        except pyscopg2.OperationalError as e:
+            logging.error('Unable to retreive data from {0}'.format(tablename))
+            pass
+    if request.method == 'POST':
+        try:
+            cur.execute("INSERT INTO {0} VALUES (3);".format(tablename))
+            return "Inserted data into {0}".format(tablename)
+        except pyscopg2.OperationalError as e:
+            logging.error('Unable to insert data into {0}'.format(tablename))
+            pass
+
+
+@app.route("/tables")
+def tables_print():
+    try:
+        cur.execute("SELECT * FROM pg_catalog.pg_tables");
+    except psycopg2.OperationalError as e:
+        logging.error('Unable to connect!\n{0}').format(e)
+        pass
+    res = ""
+    for record in cur:
+        res = res + ", " + str(record)
+    return res
 
 @app.route("/path", endpoint='func2')
 def func2():
